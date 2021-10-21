9c88d59e9cd43d99c59833638dcbfa3092a6076a
Jairo-Arcy <jairo_tunisse@outlook.com>
2021-10-20 22:26:51 -0300

    improviments :heavy_check_mark:

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+//import React from 'react'
+//import { Home } from './pages/Home';
+import './styles/global.scss'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom'
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
+//import { auth, firebase } from './services/firebase'
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom}></Route>
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
